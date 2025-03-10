44ea914d85127ef371f9a18539f888c3e023aaf4
spar5an <star.vs.blue@gmail.com>
2025-03-10 18:48:36 +0000

    working on KNN tools

--- Assembly Code/KNN_tools.s
+++ Assembly Code/KNN_tools.s
@@ -1,7 +1,10 @@
 #include <xc.inc>
 ;this file will contain all the tools needed for KNN
 
-extrn	long_subtract, long_compare, NUM1, NUM2, RESULT
+extrn	long_add, long_subtract, long_compare, NUM1, NUM2, RESULT
+
+;NONE OF THIS CODE HAS BEEN TESTED
+global	calculate_distance, point_1, point_2
     
 psect	udata_acs
 point_1:    ds	3
@@ -9,20 +12,72 @@ point_2:    ds	3
 parameter_1:	ds  1
 parameter_2:	ds  1
 total_distance: ds  3
-temp:	ds  1;this is useful for flipping w
+temp:	ds  1;this is useful for flipping w and squaring
+param_counter:	ds  1
 
     
 psect	KNN_code, class=CODE
 flip_w:
+    movwf   temp, A
+    movlw   0xff, A
+    subwf   temp, A
+    return
     
-    
-calculate_distance:
+calculate_difference:
     ;takes 2 parameters 1 byte each and returns distance
-    
     movf    parameter_1, W
     subwf   parameter_2, W
-    btfsc   STATUS, 0
-    sublw   0xff    ;this will flip the distance
+    btfss   STATUS, 0
+    call    flip_w
+    return  ;output is stored in w
+    
+calculate_distance:
+    movlw   0x00
+    movwf   total_distance, A
+    
+    movlw   0x03
+    movwf   param_counter, A
+    
+    ;going to load point_1 into fsr1, point_2 to fsr2
+    lfsr    1, point_1
+    lfsr    2, point_2
+    
+distance_loop:
+    movff   POSTINC1, parameter_1
+    movff   POSTINC2, parameter_2
+    call    calculate_difference
+    
+    movwf   temp, A
+    mulwf   temp
+    
+    movff   total_distance, NUM1
+    movff   total_distance+1, NUM1+1
+    movff   total_distance+2, NUM1+2
+    
+    movff   PRODH, NUM2+1
+    movff   PRODL, NUM2+2
+    
+    call    long_add
+    
+    movff   RESULT, total_distance
+    movff   RESULT+1, total_distance+1
+    movff   RESULT+2, total_distance+2
+    
+    
+    
+    decfsz  param_counter, f
+    bra	    distance_loop
+    
+    return
+    
+    
+    
+    
+    
+    
+    
+
+    
     
     
     
--- Assembly Code/main.s
+++ Assembly Code/main.s
@@ -3,6 +3,7 @@
 extrn	setup_data, load_data, load_labels
 
 psect	udata_acs
+k:  ds	1
 predict_point:	ds  3; this is going to  be an example point to classify
 	
 	
@@ -20,6 +21,17 @@ read:
 	call	load_data
 	call	load_labels
 	
+;test for tools
+	movlw	0x96
+	movwf	predict_point
+	movlw	0xfa
+	movwf	predict_point+1
+	movlw	0x1e
+	movwf	predict_point+2
+	;this is a point that should be classified as zero
+	
+	
+
 
 	goto	$
 	
